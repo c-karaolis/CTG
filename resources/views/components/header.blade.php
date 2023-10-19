9d8a6b6a467ae188812958e6a1cacdae1ab3ca91
Constantinos Karaolis <kostaskaraolis@gmail.com>
2023-10-19 15:55:54 +0300

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,34 @@
+<x-navbar></x-navbar>
+{{-- background: rgb(22,107,83);
+background: linear-gradient(90deg, rgba(22,107,83,1) 0%, rgba(33,80,77,0.9837184873949579) 44%, rgba(30,127,173,1) 100%); --}}
+<section class="w-full px-6 pb-12 antialiased bg-white">
+    <div class="mx-auto max-w-7xl">
+
+
+        <!-- Main Hero Content -->
+        <div
+            class="container max-w-sm py-4 mx-auto mt-px text-left md:py-32 sm:max-w-md md:max-w-lg sm:px-4 md:max-w-none md:text-center">
+            <h1 {{-- color: aliceblue; --}}
+                class="text-3xl font-bold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:text-4xl md:text-7xl lg:text-8xl ">
+                We Love Playing <br class="hidden sm:block"> Games On Tables</h1>
+            <div class="mx-auto mt-5 text-gray-400 md:mt-8 md:max-w-lg md:text-center md:text-xl">The most pationate
+                tabletop community that ever existed on the island of Cyprus!</div>
+            <div
+                class="flex flex-col items-center justify-center mt-8 space-y-4 text-center sm:flex-row sm:space-y-0 sm:space-x-4">
+                <span class="relative inline-flex w-full md:w-auto">
+                    <a href="#_"
+                        class="inline-flex items-center justify-center w-full px-8 py-4 text-base font-medium leading-6 text-white bg-gray-900 border border-transparent rounded-full xl:px-10 md:w-auto hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800">
+                        Stay Updated
+                    </a>
+                </span>
+                <span class="relative inline-flex w-full md:w-auto">
+                    <a href="#_"
+                        class="inline-flex items-center justify-center w-full px-8 py-4 text-base font-medium leading-6 text-gray-900 bg-gray-100 border border-transparent rounded-full xl:px-10 md:w-auto hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-200">Learn
+                        More</a>
+                </span>
+            </div>
+        </div>
+        <!-- End Main Hero Content -->
+
+    </div>
+</section>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<x-app>
+
+    <section class="h-[2000px] testStyle"></section>
+</x-app>
+
+<style>
+    .testStyle {
+        background-color: green;
+    }
+</style>

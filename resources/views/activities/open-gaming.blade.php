87ee1e9222276ae6f728e4aec9f71757bd0cd4a4
Constantinos Karaolis <kostaskaraolis@gmail.com>
2023-12-18 12:50:18 +0200

    fixed header logos for activity pages

--- resources/views/activities/open-gaming.blade.php
+++ resources/views/activities/open-gaming.blade.php
@@ -3,7 +3,7 @@
         <div class="hero-transparent-gradient-background">
             <div class="flex justify-center mx-auto max-w-7xl">
                 <div
-                    class="container max-w-sm py-20 mx-auto mt-px text-left md:py-32 sm:max-w-md sm:px-4 md:max-w-none md:text-center">
+                    class="container max-w-sm px-4 py-20 mx-auto mt-px text-left md:py-32 sm:max-w-md md:max-w-none md:text-center">
                     <img class="mx-auto" src="{{ asset('/images/logos/CTGLogoWhite.svg') }}"
                         alt="Cyprus Tabletop Gaming full logo">
                 </div>

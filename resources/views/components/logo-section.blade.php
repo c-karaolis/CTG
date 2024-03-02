7011d5bce6aeb4706a65abeb4c9b48f374e93337
Constantinos Karaolis <kostaskaraolis@gmail.com>
2024-03-02 15:16:48 +0200

    updated logo section with new sponsors

--- resources/views/components/logo-section.blade.php
+++ resources/views/components/logo-section.blade.php
@@ -11,6 +11,23 @@
                     </a>
                 </li>
                 <li>
+                    <a href="https://www.kemanesprintshop.com/" target="_blank">
+                        <img src="{{ asset('images/logos/kemanes-logo.png') }}" alt="" width="148px">
+                    </a>
+                </li>
+
+                <li>
+                    <a href="https://antcomics.com.cy/" target="_blank">
+                        <img src="{{ asset('images/logos/antcomics-logo.png') }}" alt="" width="140px">
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.uol.ac.cy/" target="_blank">
+                        <img src="{{ asset('images/logos/uol-logo.svg') }}" alt="" width="148px">
+                    </a>
+                </li>
+
+                {{-- <li>
                     <a href="https://www.alphamega.com.cy/" target="_blank">
                         <img src="{{ asset('images/logos/alphamega-logo.png') }}" alt="" width="164px">
                     </a>
@@ -24,17 +41,18 @@
                     <a href="https://cybc.com.cy/" target="_blank">
                         <img src="{{ asset('images/logos/rik-logo.png') }}" alt="" width="100px">
                     </a>
-                </li>
+                </li> --}}
                 <li>
                     <a href="https://www.facebook.com/geekotopos/" target="_blank">
                         <img src="{{ asset('images/logos/geekotopos-logo.png') }}" alt="" width="94px">
                     </a>
                 </li>
                 <li>
-                    <a href="https://engomi.org/" target="_blank">
-                        <img src="{{ asset('images/logos/engomi-logo.png') }}" alt="" width="148px">
+                    <a href="https://www.lakatamia.org.cy/" target="_blank">
+                        <img src="{{ asset('images/logos/lakatamia-logo.png') }}" alt="" width="100px">
                     </a>
                 </li>
+
             </ul>
         </div>
     </div>
